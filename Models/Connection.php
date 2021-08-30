60686c248c84766345fd938b6b2b03901dc4cd16
HiuberMorente <hiuberr@gmail.com>
2021-08-29 21:53:23 -0600

    complete

--- Models/Connection.php
+++ Models/Connection.php
@@ -19,10 +19,16 @@
 //         ""
 //      );
       
+//      $link = new PDO(
+//         "mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_fbe38b75ba63a97",
+//         "bad56afeeec69e",
+//         "c25f632e"
+//      );
+      
       $link = new PDO(
-         "mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_fbe38b75ba63a97",
-         "bad56afeeec69e",
-         "c25f632e"
+         "ec2-3-222-11-129.compute-1.amazonaws.com;dbname=d9olkh54oodcas",
+         "sdyoobmitxkzkm",
+         "6cce158829788c5aafc72a20d68a43db78b87bbf75a6a9a434b161872a68ece8"
       );
 
       $link->exec("set names utf8");
