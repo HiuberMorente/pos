89f77503843d6f64ce839a3d3f44308cdaec95b6
HiuberMorente <hiuberr@gmail.com>
2021-08-29 22:39:20 -0600

    fixing bugs

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5583f731c6df2f01b61407d4f1405d2d0dbf3e9f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5583f731c6df2f01b61407d4f1405d2d0dbf3e9f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
