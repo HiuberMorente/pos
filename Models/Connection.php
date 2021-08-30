5583f731c6df2f01b61407d4f1405d2d0dbf3e9f
HiuberMorente <hiuberr@gmail.com>
2021-08-29 22:10:32 -0600

    complete

--- Models/Connection.php
+++ Models/Connection.php
@@ -19,17 +19,13 @@
 //         ""
 //      );
       
-//      $link = new PDO(
-//         "mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_fbe38b75ba63a97",
-//         "bad56afeeec69e",
-//         "c25f632e"
-//      );
-      
       $link = new PDO(
-         "postgres:host=ec2-3-222-11-129.compute-1.amazonaws.com;port=5432;dbname=d9olkh54oodcas",
-         "sdyoobmitxkzkm",
-         "6cce158829788c5aafc72a20d68a43db78b87bbf75a6a9a434b161872a68ece8"
+         "mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_fbe38b75ba63a97",
+         "bad56afeeec69e",
+         "c25f632e"
       );
+      
+     
 
       $link->exec("set names utf8");
 
